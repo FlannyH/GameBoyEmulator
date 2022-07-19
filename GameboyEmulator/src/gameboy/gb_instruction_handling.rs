bdd1860de88e152a528bd458bd3e43bf95b9ef1e
Flan <flannyha@gmail.com>
2022-07-19 11:39:51 +0200

    Got my basic test rom workin + bootrom

--- GameboyEmulator/src/gameboy/gb_instruction_handling.rs
+++ GameboyEmulator/src/gameboy/gb_instruction_handling.rs
@@ -2,16 +2,26 @@ use super::GameBoy;
 
 impl GameBoy {
     pub(in crate) fn run_frame(&mut self) {
-        for i in 0..96 {
+        for i in 0..5120 {
             self.process_next_instruction();
             //self.print_reg_state();
+            self.run_ppu_cycle();
+            self.run_ppu_cycle();
         }
     }
 
     pub(in super::super) fn process_next_instruction(&mut self) {
+        //self.times[self.last_opcode as usize] = self.curr_cycles_to_wait as u8;
+        //self.curr_cycles_to_wait = 0;
+        if (self.curr_cycles_to_wait > 0) {
+            self.curr_cycles_to_wait -= 1;
+        }
         // Read byte from PC
         let opcode = self.fetch_next_byte_from_pc();
-        //println!("Opcode: ${:02X}", opcode);
+        self.last_opcode = opcode;
+        if (self.rom_chip_enabled == false) {
+            //println!("Opcode: ${:02X}, PC: ${:04X}", opcode, self.pc);
+        }
 
         // Pass it to a bunch of functions, let them handle it. If none of them handle it, this is an invalid opcode, and we should hang.
         if self.handle_misc_instructions(opcode) {
@@ -44,7 +54,7 @@ impl GameBoy {
             0x10 => todo!(), // STOP
             0x17 => self.reg_a = self.rl(self.reg_a),
             0x1F => self.reg_a = self.rr(self.reg_a),
-            0x76 => todo!(), // HALT
+            //0x76 => self.curr_cycles_to_wait = 40000000, // HALT
             0xCB => {
                 self.handle_prefixed_instructions(opcode);
             } // CB - prefixed instructions mostly for bit shifting, setting, and clearing
